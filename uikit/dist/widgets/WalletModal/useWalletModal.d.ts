e7438d77d0b4016ffd5e963672fb93d279592fa5
mahirocoko <cokawa.mahiro@gmail.com>
2021-06-25 13:56:55 +0700

    refactor: Remove uikit

--- uikit/dist/widgets/WalletModal/useWalletModal.d.ts
+++
@@ -1,7 +0,0 @@
-import { Login } from "./types";
-interface ReturnType {
-    onPresentConnectModal: () => void;
-    onPresentAccountModal: () => void;
-}
-declare const useWalletModal: (login: Login, logout: () => void, account?: string) => ReturnType;
-export default useWalletModal;
