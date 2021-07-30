a960d4a7a4ca0bbd56984c22b42a6affec6f51a1
korrio <iamnotkorr@gmail.com>
2021-07-30 19:58:19 +0700

    finalize

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -6,16 +6,30 @@ const config: MenuEntry[] = [
     icon: 'HomeIcon',
     href: 'https://royalcats.finance',
   },
-  {
-    label: 'Exchange',
-    href: '/swap',
-    icon: 'ExchangeIcon',
-  },
-  {
-    label: 'Liquidity',
-    href: '/pool',
-    icon: 'LiquidityIcon',
-  },
+  // {
+  //   label: 'Exchange',
+  //   href: '/swap',
+  //   icon: 'ExchangeIcon',
+  // },
+  // {
+  //   label: 'Liquidity',
+  //   href: '/pool',
+  //   icon: 'LiquidityIcon',
+  // },
+      {
+      label: 'Trade',
+      icon: 'TradeIcon',
+      items: [
+        {
+          label: 'Exchange',
+          href: 'https://exchange.royalcats.finance',
+        },
+        {
+          label: 'Liquidity',
+          href: 'https://exchange.royalcats.finance/#/pool',
+        },
+      ],
+    },
   {
     label: 'Farms',
     icon: 'FarmIcon',
@@ -25,11 +39,15 @@ const config: MenuEntry[] = [
       color: 'success',
     },
   },
-  // {
-  //   label: 'Pools',
-  //   icon: 'PoolIcon',
-  //   href: '/pools',
-  // },
+  {
+    label: 'Pools',
+    icon: 'PoolIcon',
+    href: 'https://royalcats.finance/pools',
+    status: {
+      text: "NEW",
+      color: 'success',
+    },
+  },
   // {
   //   label: 'Social',
   //   icon: 'HamburgerIcon',
@@ -56,6 +74,23 @@ const config: MenuEntry[] = [
   //     },
   //   ],
   // },
+  {
+        label: 'More',
+        icon: 'MoreIcon',
+        items: [{
+                label: 'Github',
+                href: 'https://github.com/royalcatslotto',
+            },
+            {
+                label: 'Docs',
+                href: 'https://docs.royalcats.finance/',
+            },
+            {
+                label: 'Blog',
+                href: 'https://royalcatslotto.medium.com/',
+            },
+        ],
+    },
 ]
 
 export default config
